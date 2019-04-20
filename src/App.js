f1177cd35300cf1d83c2ecaab1c2fbaed4bc3e6b
AaronStahley <acstahle@asu.edu>
2019-04-19 18:51:44 -0700

    added a route /test to put sample code that will exsist on the main production but conserve the construction page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import gear from './gear_black.png';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={gear} className="gear-1" alt="gear-1" />
-          <p>
-            Website Under
-            Construction
-          </p>
-          <a
-            className="App-link"
-            href="https://github.com/AaronStahley/aaronstahley.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Follow Project
-          </a>
-        </header>
 
+    /**
+     * Displays no mater what route. Should hold nav 
+     * and footer info. 
+     */
+    return (
+      <Router>   
+        <Route exact path="/" component={Home} />
+        <Route path="/test" component={test} />
+      </Router> 
+    );
+  }
+}
+
+/**
+ * Holds the consturction page for now. 
+ */
+function Home() { 
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src={gear} className="gear-1" alt="gear-1" />
+      <p>
+        Website Under
+        Construction
+      </p>
+      <a
+        className="App-link"
+        href="https://github.com/AaronStahley/aaronstahley.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Follow Project
+    </a>
+    </header>
+    </div>
+  );
+}
+
+/**
+ * Used to test on a deployed site. 
+ */
+function test() {
+  return (
+    <div>
         <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
         <p>
           <label>Email: <input type="text" name="name" /></label>
@@ -34,9 +63,8 @@ class App extends Component {
           <button type="submit">Send</button>
         </p>
         </form>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
